89e870f810ccf358afe7d52dfe45fdd283e4254f
EmperorBongo <bbr.bronsonb@gmail.com>
2023-12-21 13:31:51 -0700

    hello

--- server.js
+++ server.js
@@ -3,4 +3,24 @@ const app = express();
 
 app.use(express.static(`${__dirname}/public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '87ae6b5f034948b0aa7fa475827d8f1e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+    // Expected output: ReferenceError: nonExistentFunction is not defined
+    // (Note: the exact output may be browser-dependent)
+  }
+  
+
 app.listen(4000, () => console.log("up on 4000"))
